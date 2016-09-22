f2e79dbf963b310a851331f9f09a9c947497daad
YotaKikuchi <mr.suidou@gmail.com>
2016-09-23 00:20:08 +0900

    初回
    1回目のコミット

---
+++ Assets/NumCounterScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NumCounterScript : MonoBehaviour {
+
+	Text numText;
+	int initNum = 0;
+	int currentNum;
+
+	// Use this for initialization
+	void Start () {
+		numText = this.GetComponent<Text>();
+		UpdataText(initNum);
+		currentNum = initNum;
+	}
+
+	void UpdataText(int n){
+		numText.text = n.ToString();
+	}
+
+	public void AddNum(int n){
+		currentNum += n;
+		UpdataText(currentNum);
+	}
+
+}
